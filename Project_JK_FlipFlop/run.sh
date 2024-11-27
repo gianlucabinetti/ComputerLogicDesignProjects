1d90ed63913c90636586eef9c27625779d6526f2
gianlucabinetti <Gianlucabinetti2001@gmail.com>
2024-11-27 01:05:52 -0500

    Delete Project_JK_FlipFlop directory

--- Project_JK_FlipFlop/run.sh
+++
@@ -1,7 +0,0 @@
-cd /home/runner
-export PATH=/usr/bin:/bin:/tool/pandora64/bin:/usr/share/Riviera-PRO/bin:/usr/local/bin
-export RIVIERA_HOME=/usr/share/Riviera-PRO
-export CPLUS_INCLUDE_PATH=/usr/share/Riviera-PRO/interfaces/include
-export ALDEC_LICENSE_FILE=27009@10.116.0.5
-export HOME=/home/runner
-vlib work && vcom '-2019' '-o' design.vhd testbench.vhd  && vsim -c -do "vsim TB_JK_FlipFlop; run 120 ns; exit"  ; echo 'Creating result.zip...' && zip -r /tmp/tmp_zip_file_123play.zip . && mv /tmp/tmp_zip_file_123play.zip result.zip
